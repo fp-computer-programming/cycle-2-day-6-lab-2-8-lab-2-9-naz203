9999975306f51160fe05b0ff34d3353b666fd974
naz <p24nthompson@fairfieldprep.org>
2023-09-26 10:56:02 -0400

    lab2_8 all done

--- lab2-8.py
+++ lab2-8.py
@@ -5,3 +5,14 @@ and between 9-11 point to win a bronze medal. A team does not earn a medal if th
 Write a program using nested if else statements where a user can input the number of points a team scored and the medal that they earned is displayed.
 
 """
+a = input('how many point a team scored')
+a= int(a)
+if a>=15: print('earned gold medal')
+else :
+    if 12<=a<14:
+        print ('earned silver medal')
+    else: 
+        if (9<=a<=11):
+         print('earned bronze medal')
+
+         if a<=8: print (' no meadal earned')
